7fb92383cd9d534882e91b0c4024cf6b45a1f30d
s1411122039 <abc0979775465@gmail.com>
2024-11-15 12:03:44 +0800

    1115

---
+++ server.js
@@ -0,0 +1,16 @@
+//install: node js
+//install web server package: express >npm install express
+var express = require("express");
+var server = express();
+
+//web root
+server.use(express.static(__dirname+"/css"));
+server.get("/profolio", (req,res)=>{       
+            res.send("Profolio");})  
+            var DB = require("nedb-promises"); 
+            var Profolio = DB.create(__dirname+"/profolio.db");  
+            Profolio.insert({modal: "#portfolioModal1", imgSrc:"roundicons.png", heading:"Round Icons", text:"Graphic Design"})
+
+server.listen(80, ()=>{
+    console.log("Server is running at port 80.");
+})
